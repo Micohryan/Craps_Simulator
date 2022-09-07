54c5d72ac42f928a06bc605d2f05720c0f0e8410
Micohryan <micohryan@gmail.com>
2022-09-06 18:00:47 -0700

    adding py and html files

---
+++ SimpleCalculator.py
@@ -0,0 +1,20 @@
+def addnum(firstnum,secondnum) :
+    print("hello")
+
+def subnum() :
+    print("hello")
+
+def multnum() :
+    print("hello")
+
+def divnum() :
+    print("hello")
+
+def main():
+    firstnum = input()
+    operator = input()
+    secondnum = input()
+
+if __name__ == "__main__":
+    main()
+#hello
