c0f23f6c4792834646ae335897d4fc8007c6f5d6
Micohryan <micohryan@gmail.com>
2022-09-11 18:26:12 -0700

    first version craps

--- cube.js
+++ cube.js
@@ -4,6 +4,9 @@ let bankroll = 20;
 document.getElementById("bankroll").innerHTML = "bankroll is:" + bankroll;
 rollBtn.disabled = true;
 let submitBtn = document.querySelector('.submitBtn');
+let inputbar = document.getElementById("bet")
+let first_roll = true;
+let point = 0;
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -17,10 +20,45 @@ function toggleClass(die) {
 }
 
 function rollDice() {
+  let sum = 0;
   cube.forEach((die) => {
     toggleClass(die);
-    die.dataset.roll = getRandomInt(1,7);
+    let dice_roll = getRandomInt(1,7);
+    die.dataset.roll = dice_roll;
+    sum += dice_roll
   });
+  return sum;
+}
+
+function second_phase(rolled_point) {
+  roll = rollDice()
+  if (roll = rolled_point) {
+    first_roll = true;
+    return "WIN";
+  }
+  if (roll = 7) {
+    first_roll = true;
+    return "LOSE";
+  }
+}
+
+function first_phase(roll){
+  if (first_roll) {
+    roll = roll_dice();
+    if (roll === 7 || roll === 11) {
+      return "WIN";
+    }
+    else if (roll === 2 || roll === 3 || roll === 12 ) {
+      return "LOSE";
+    }
+    else {
+      point = roll;
+      first_roll = false;
+    }
+  }
+  else {
+    second_phase(point);
+  }
 }
 
 function validateinput() {
@@ -35,10 +73,12 @@ function validateinput() {
     text = "Valid bet please roll"
     document.getElementById("demo").innerHTML = text;
     rollBtn.disabled = false;
+    submitBtn.style.visibility ="hidden";
+    inputbar.style.visibility = "hidden";
     return true;
   }
 }
 // set initial side
 rollDice();
 submitBtn.addEventListener("click", validateinput);
-rollBtn.addEventListener("click", rollDice);
+rollBtn.addEventListener("click", first_phase);
