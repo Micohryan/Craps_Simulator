a4aa82e5a90d06d12dbbf4cb30924994b391054c
Micohryan <micohryan@gmail.com>
2022-09-14 16:44:30 -0700

    bankroll is now calculated

--- cube.js
+++ cube.js
@@ -7,6 +7,7 @@ let submitBtn = document.querySelector('.submitBtn');
 let inputbar = document.getElementById("bet")
 let first_roll = true;
 let point = 0;
+let bet_size = 0;
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -21,7 +22,7 @@ function toggleClass(die) {
 
 function rollDice() {
   let sum = 0;
-  cube.forEach((die) => {
+  cube.forEach((die) => { 
     toggleClass(die);
     let dice_roll = getRandomInt(1,7);
     die.dataset.roll = dice_roll;
@@ -29,31 +30,53 @@ function rollDice() {
   });
   return sum;
 }
+function winner() { /* win lose functions can be combined */
+  submitBtn.style.visibility ="visible";
+  inputbar.style.visibility = "visible";
+  const bets = parseInt(bet_size);
+  bankroll = bankroll + bets;
+  document.getElementById("bankroll").innerHTML = "bankroll is:" + bankroll;
+}
+
+function lose() {
+  submitBtn.style.visibility ="visible";
+  inputbar.style.visibility = "visible";
+  bankroll -= bet_size;
+  document.getElementById("bankroll").innerHTML = "bankroll is:" + bankroll;
+}
 
 function second_phase(rolled_point) {
-  roll = rollDice()
-  if (roll = rolled_point) {
+  roll = rollDice();
+  console.log(roll);
+  if (roll === rolled_point) {
     first_roll = true;
-    return "WIN";
+    document.getElementById("demo").innerHTML = "That's a Dubby Wubby";
+    winner();
   }
-  if (roll = 7) {
+  if (roll === 7) {
     first_roll = true;
-    return "LOSE";
+    document.getElementById("demo").innerHTML = "That's a L";
+    lose();
   }
+  document.getElementById("demo").innerHTML = "Please click roll dice again";
 }
 
 function first_phase(roll){
+  console.log(first_roll);
   if (first_roll) {
-    roll = roll_dice();
+    roll = rollDice();
     if (roll === 7 || roll === 11) {
-      return "WIN";
+      document.getElementById("demo").innerHTML = "That's a Dubby Wubby";
+      winner();
     }
     else if (roll === 2 || roll === 3 || roll === 12 ) {
-      return "LOSE";
+      document.getElementById("demo").innerHTML = "That's a L";
+      lose();
     }
     else {
       point = roll;
       first_roll = false;
+      document.getElementById("demo").innerHTML = "Point is now " + point + " Please click roll dice again";
     }
   }
   else {
@@ -63,6 +86,7 @@ function first_phase(roll){
 
 function validateinput() {
   let numInput = document.getElementById("bet").value;
+  bet_size = numInput;
   let text;
   if (isNaN(numInput) || numInput < 1 || numInput > bankroll) {
     text = "Invalid Input"
